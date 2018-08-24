3f2467d10c8e7bc4dd854978401c1cfaf11ec37c
kazimsanlav <kazimsanlav@gmail.com>
2018-08-25 02:27:17 +0300

    add cam

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
+let cam, can;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	createCapture(VIDEO);
+	can = createCanvas(windowWidth, windowHeight);
+	background(100);
+	cam = createCapture(VIDEO);
+	cam.hide();
+	// cam.size(windowWidth/2-10, 400);
 }
 
 function draw() {
+	
+	image(cam, 0, 0, width, height);
+	noStroke();
+	fill(random(200,255),random(100,200));
+	ellipse(random(width), random(height), random(10,30));
+}
 
+setInterval(changeColor,1000);
+
+function changeColor(){
+	tint(random(255),random(255),random(255));
 }
